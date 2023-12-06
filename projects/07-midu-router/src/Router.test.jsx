9570985e21c80e96690829baab0986ed072f679c
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-12-06 13:14:05 +0000

    feat(07): add tests & dependencies

---
+++ projects/07-midu-router/src/Router.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+describe('Router', () => {
+  it('should work', () => {
+    const app = render
+  })
+})
